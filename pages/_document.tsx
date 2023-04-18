8653784cf9cd958b1431ff95fb2c021d745d8212
flaviusburca <flaviusburca@users.noreply.github.com>
2023-04-18 15:18:20 +0300

    Arthera-specific changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -41,11 +41,11 @@ class MyDocument extends Document {
           <link rel="icon" sizes="16x16" type="image/png"href="/static/favicon-16x16.png"/>
           <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png"/>
           <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5"/>
-          <meta property="og:title" content="Blockscout: A block explorer designed for a decentralized world."/>
+          <meta property="og:title" content="Arthera: A different future for Web3"/>
           <meta
             property="og:description"
             // eslint-disable-next-line max-len
-            content="Blockscout is the #1 open-source blockchain explorer available today. 100+ chains and counting rely on Blockscout data availability, APIs, and ecosystem tools to support their networks."
+            content="Arthera blockchain explorer powered by Blockscout."
           />
           <meta property="og:image" content={ appConfig.baseUrl + '/static/og.png' }/>
           <meta property="og:site_name" content="Blockscout"/>
