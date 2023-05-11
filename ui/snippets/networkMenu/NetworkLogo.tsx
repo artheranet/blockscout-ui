2085b647eb05f33f5f4cdf63e6969a7f5647d0b4
flaviusburca <flaviusburca@users.noreply.github.com>
2023-05-11 08:45:02 +0300

    Arthera-specific changes

--- ui/snippets/networkMenu/NetworkLogo.tsx
+++ ui/snippets/networkMenu/NetworkLogo.tsx
@@ -1,103 +1,88 @@
-import { Icon, Box, Image, useColorModeValue } from '@chakra-ui/react';
+import { Icon, Box, Image, useColorModeValue, Skeleton } from '@chakra-ui/react';
 import { route } from 'nextjs-routes';
 import React from 'react';
 
 import appConfig from 'configs/app/config';
-import smallLogoPlaceholder from 'icons/networks/icons/placeholder.svg';
-import logoPlaceholder from 'icons/networks/logos/blockscout.svg';
-import ASSETS from 'lib/networks/networkAssets';
+import iconPlaceholder from 'icons/networks/icon-placeholder.svg';
+import logoPlaceholder from 'icons/networks/logo-placeholder.svg';
 
 interface Props {
-  isCollapsed?: boolean;
-  onClick?: (event: React.SyntheticEvent) => void;
+    isCollapsed?: boolean;
+    onClick?: (event: React.SyntheticEvent) => void;
 }
 
-const NetworkLogo = ({ isCollapsed, onClick }: Props) => {
-  const logoColor = useColorModeValue('blue.600', 'white');
-  const href = route({ pathname: '/' });
-  const [ isLogoError, setLogoError ] = React.useState(false);
-  const [ isSmallLogoError, setSmallLogoError ] = React.useState(false);
+const LogoFallback = ({ isCollapsed, isSmall }: { isCollapsed?: boolean; isSmall?: boolean }) => {
+    const field = isSmall ? 'icon' : 'logo';
+    const logoColor = useColorModeValue('blue.600', 'white');
 
-  const style = useColorModeValue({}, { filter: 'brightness(0) invert(1)' });
+    const display = isSmall ? {
+        base: 'none',
+        lg: isCollapsed === false ? 'none' : 'block',
+        xl: isCollapsed ? 'block' : 'none',
+    } : {
+        base: 'block',
+        lg: isCollapsed === false ? 'block' : 'none',
+        xl: isCollapsed ? 'none' : 'block',
+    };
 
-  const handleSmallLogoError = React.useCallback(() => {
-    setSmallLogoError(true);
-  }, []);
-
-  const handleLogoError = React.useCallback(() => {
-    setLogoError(true);
-  }, []);
-
-  const logoEl = (() => {
-    const fallbackLogoSrc = appConfig.network.type ? ASSETS[appConfig.network.type]?.logo : undefined;
-    const fallbackSmallLogoSrc = appConfig.network.type ? ASSETS[appConfig.network.type]?.smallLogo || ASSETS[appConfig.network.type]?.icon : undefined;
-
-    const logo = appConfig.network.logo;
-    const smallLogo = appConfig.network.smallLogo;
-
-    const fallbackLogo = (
-        <Icon
-            as={ fallbackLogoSrc || logoPlaceholder }
-            width="auto"
-            height="100%"
-            color={ fallbackLogoSrc ? undefined : logoColor }
-            display={{ base: 'block', lg: isCollapsed === false ? 'block' : 'none', xl: isCollapsed ? 'none' : 'block' }}
-            style={ style }
-        />
-    );
-    const fallbackSmallLogo = (
-        <Icon
-            as={ fallbackSmallLogoSrc || smallLogoPlaceholder }
-            width="auto"
-            height="100%"
-            color={ fallbackSmallLogoSrc ? undefined : logoColor }
-            display={{ base: 'none', lg: isCollapsed === false ? 'none' : 'block', xl: isCollapsed ? 'block' : 'none' }}
-            style={ style }
-        />
-    );
+    if (appConfig.network[field].default) {
+        return <Skeleton w="100%" borderRadius="sm" display={ display }/>;
+    }
 
     return (
-        <>
-          { /* big logo */ }
-          <Image
-              w="auto"
-              h="100%"
-              src={ logo }
-              display={{ base: 'block', lg: isCollapsed === false ? 'block' : 'none', xl: isCollapsed ? 'none' : 'block' }}
-              alt={ `${ appConfig.network.name } network logo` }
-              fallback={ isLogoError || !logo ? fallbackLogo : undefined }
-              onError={ handleLogoError }
-          />
-          { /* small logo */ }
-          <Image
-              w="auto"
-              h="100%"
-              src={ smallLogo }
-              display={{ base: 'none', lg: isCollapsed === false ? 'none' : 'block', xl: isCollapsed ? 'block' : 'none' }}
-              alt={ `${ appConfig.network.name } network logo` }
-              fallback={ isSmallLogoError || !smallLogo ? fallbackSmallLogo : undefined }
-              onError={ handleSmallLogoError }
-          />
-        </>
+        <Icon
+            as={ isSmall ? iconPlaceholder : logoPlaceholder }
+            width="auto"
+            height="100%"
+            color={ logoColor }
+            display={ display }
+        />
     );
-  })();
+};
 
-  return (
-      // TODO switch to <NextLink href={ href } passHref> when main page for network will be ready
-      <Box
-          as="a"
-          href={ href }
-          width={{ base: 'auto', lg: isCollapsed === false ? '240px' : '40px', xl: isCollapsed ? '30px' : '240px' }}
-          height={{ base: '30px', lg: isCollapsed === false ? '40px' : '30px', xl: isCollapsed ? '30px' : '40px' }}
-          display="inline-flex"
-          overflow="hidden"
-          onClick={ onClick }
-          flexShrink={ 0 }
-          aria-label="Link to main page"
-      >
-        { logoEl }
-      </Box>
-  );
+const NetworkLogo = ({ isCollapsed, onClick }: Props) => {
+
+    const logoSrc = useColorModeValue(appConfig.network.logo.default, appConfig.network.logo.dark || appConfig.network.logo.default);
+    const iconSrc = useColorModeValue(appConfig.network.icon.default, appConfig.network.icon.dark || appConfig.network.icon.default);
+    const darkModeFilter = { filter: 'brightness(0) invert(1)' };
+    const logoStyle = useColorModeValue({}, !appConfig.network.logo.dark ? darkModeFilter : {});
+    const iconStyle = useColorModeValue({}, !appConfig.network.icon.dark ? darkModeFilter : {});
+
+    return (
+        // TODO switch to <NextLink href={ href } passHref> when main page for network will be ready
+        <Box
+            as="a"
+            href={ route({ pathname: '/' }) }
+            width={{ base: 'auto', lg: isCollapsed === false ? '120px' : '30px', xl: isCollapsed ? '30px' : '120px' }}
+            height={{ base: '20px', lg: isCollapsed === false ? '20px' : '30px', xl: isCollapsed ? '30px' : '20px' }}
+            display="inline-flex"
+            overflow="hidden"
+            onClick={ onClick }
+            flexShrink={ 0 }
+            aria-label="Link to main page"
+        >
+            { /* big logo */ }
+            <Image
+                w="auto"
+                h="100%"
+                src={ logoSrc }
+                alt={ `${ appConfig.network.name } network logo` }
+                fallback={ <LogoFallback isCollapsed={ isCollapsed }/> }
+                display={{ base: 'block', lg: isCollapsed === false ? 'block' : 'none', xl: isCollapsed ? 'none' : 'block' }}
+                style={ logoStyle }
+            />
+            { /* small logo */ }
+            <Image
+                w="auto"
+                h="100%"
+                src={ iconSrc }
+                alt={ `${ appConfig.network.name } network logo` }
+                fallback={ <LogoFallback isCollapsed={ isCollapsed } isSmall/> }
+                display={{ base: 'none', lg: isCollapsed === false ? 'none' : 'block', xl: isCollapsed ? 'block' : 'none' }}
+                style={ iconStyle }
+            />
+        </Box>
+    );
 };
 
 export default React.memo(NetworkLogo);
