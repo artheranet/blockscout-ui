f776580a516aab0f06f321e7c6f453fe482e204e
tom goriunov <tom@ohhhh.me>
2023-08-16 02:28:00 +0300

    refactor page layout components (#1087)
    * update csv export text
    
    * fix pw tests
    
    * base layout and customization for home page and search results page
    
    * refactor rest of the pages
    
    * refactor AppError
    
    * move header padding to layout
    
    * move nextjs utils to separate folder
    
    * combine PageServer and Page
    
    * make useQueryClientConfig hook
    
    * add tests
    
    * remove unused var
    
    * fix envs for pw
    
    * fix pw tests
    
    (cherry picked from commit c58a3fdf6319df1bfc7083b2c773c212097342b1)

--- ui/pages/Home.pw.tsx
+++ ui/pages/Home.pw.tsx
@@ -10,126 +10,133 @@ import contextWithEnvs from 'playwright/fixtures/contextWithEnvs';
 import TestApp from 'playwright/TestApp';
 import buildApiUrl from 'playwright/utils/buildApiUrl';
 import * as configs from 'playwright/utils/configs';
-import insertAdPlaceholder from 'playwright/utils/insertAdPlaceholder';
 
 import Home from './Home';
 
 test.describe('default view', () => {
-  let component: Locator;
+    let component: Locator;
 
-  test.beforeEach(async({ page, mount }) => {
-    await page.route(buildApiUrl('homepage_stats'), (route) => route.fulfill({
-      status: 200,
-      body: JSON.stringify(statsMock.base),
-    }));
-    await page.route(buildApiUrl('homepage_blocks'), (route) => route.fulfill({
-      status: 200,
-      body: JSON.stringify([
-        blockMock.base,
-        blockMock.base2,
-      ]),
-    }));
-    await page.route(buildApiUrl('homepage_txs'), (route) => route.fulfill({
-      status: 200,
-      body: JSON.stringify([
-        txMock.base,
-        txMock.withContractCreation,
-        txMock.withTokenTransfer,
-      ]),
-    }));
-    await page.route(buildApiUrl('homepage_chart_txs'), (route) => route.fulfill({
-      status: 200,
-      body: JSON.stringify(dailyTxsMock.base),
-    }));
+    test.beforeEach(async({ page, mount }) => {
+        await page.route(buildApiUrl('homepage_stats'), (route) => route.fulfill({
+            status: 200,
+            body: JSON.stringify(statsMock.base),
+        }));
+        await page.route(buildApiUrl('homepage_blocks'), (route) => route.fulfill({
+            status: 200,
+            body: JSON.stringify([
+                blockMock.base,
+                blockMock.base2,
+            ]),
+        }));
+        await page.route(buildApiUrl('homepage_txs'), (route) => route.fulfill({
+            status: 200,
+            body: JSON.stringify([
+                txMock.base,
+                txMock.withContractCreation,
+                txMock.withTokenTransfer,
+            ]),
+        }));
+        await page.route(buildApiUrl('homepage_chart_txs'), (route) => route.fulfill({
+            status: 200,
+            body: JSON.stringify(dailyTxsMock.base),
+        }));
 
-    component = await mount(
-      <TestApp>
-        <Home/>
-      </TestApp>,
-    );
-
-    await insertAdPlaceholder(page);
-  });
-
-  test('-@default +@dark-mode', async() => {
-    await expect(component.locator('main')).toHaveScreenshot();
-  });
-
-  test.describe('screen xl', () => {
-    test.use({ viewport: configs.viewport.xl });
-
-    test('', async() => {
-      await expect(component.locator('main')).toHaveScreenshot();
+        component = await mount(
+            <TestApp>
+                <Home/>
+            </TestApp>,
+        );
     });
-  });
+
+    test('-@default +@dark-mode', async({ page }) => {
+        await expect(component).toHaveScreenshot({
+            mask: [ page.locator(configs.adsBannerSelector) ],
+            maskColor: configs.maskColor,
+        });
+    });
+
+    test.describe('screen xl', () => {
+        test.use({ viewport: configs.viewport.xl });
+
+        test('', async({ page }) => {
+            await expect(component).toHaveScreenshot({
+                mask: [ page.locator(configs.adsBannerSelector) ],
+                maskColor: configs.maskColor,
+            });
+        });
+    });
 });
 
 test.describe('custom hero plate background', () => {
-  const IMAGE_URL = 'https://localhost:3000/my-image.png';
-  const extendedTest = test.extend({
-    context: contextWithEnvs([
-      { name: 'NEXT_PUBLIC_HOMEPAGE_PLATE_BACKGROUND', value: `no-repeat center/cover url(${ IMAGE_URL })` },
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ]) as any,
-  });
-
-  extendedTest('default view', async({ mount, page }) => {
-    await page.route(IMAGE_URL, (route) => {
-      return route.fulfill({
-        status: 200,
-        path: './playwright/giant_duck_long.jpg',
-      });
+    const IMAGE_URL = 'https://localhost:3000/my-image.png';
+    const extendedTest = test.extend({
+        context: contextWithEnvs([
+            { name: 'NEXT_PUBLIC_HOMEPAGE_PLATE_BACKGROUND', value: `no-repeat center/cover url(${ IMAGE_URL })` },
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        ]) as any,
     });
 
-    const component = await mount(
-      <TestApp>
-        <Home/>
-      </TestApp>,
-    );
+    extendedTest('default view', async({ mount, page }) => {
+        await page.route(IMAGE_URL, (route) => {
+            return route.fulfill({
+                status: 200,
+                path: './playwright/giant_duck_long.jpg',
+            });
+        });
 
-    const heroPlate = component.locator('div[data-label="hero plate"]');
+        const component = await mount(
+            <TestApp>
+                <Home/>
+            </TestApp>,
+        );
 
-    await expect(heroPlate).toHaveScreenshot();
-  });
+        const heroPlate = component.locator('div[data-label="hero plate"]');
+
+        await expect(heroPlate).toHaveScreenshot({
+            mask: [ page.locator(configs.adsBannerSelector) ],
+            maskColor: configs.maskColor,
+        });
+    });
 });
 
 // had to separate mobile test, otherwise all the tests fell on CI
 test.describe('mobile', () => {
-  test.use({ viewport: devices['iPhone 13 Pro'].viewport });
+    test.use({ viewport: devices['iPhone 13 Pro'].viewport });
 
-  test('base view', async({ mount, page }) => {
-    await page.route(buildApiUrl('homepage_stats'), (route) => route.fulfill({
-      status: 200,
-      body: JSON.stringify(statsMock.base),
-    }));
-    await page.route(buildApiUrl('homepage_blocks'), (route) => route.fulfill({
-      status: 200,
-      body: JSON.stringify([
-        blockMock.base,
-        blockMock.base2,
-      ]),
-    }));
-    await page.route(buildApiUrl('homepage_txs'), (route) => route.fulfill({
-      status: 200,
-      body: JSON.stringify([
-        txMock.base,
-        txMock.withContractCreation,
-        txMock.withTokenTransfer,
-      ]),
-    }));
-    await page.route(buildApiUrl('homepage_chart_txs'), (route) => route.fulfill({
-      status: 200,
-      body: JSON.stringify(dailyTxsMock.base),
-    }));
+    test('base view', async({ mount, page }) => {
+        await page.route(buildApiUrl('homepage_stats'), (route) => route.fulfill({
+            status: 200,
+            body: JSON.stringify(statsMock.base),
+        }));
+        await page.route(buildApiUrl('homepage_blocks'), (route) => route.fulfill({
+            status: 200,
+            body: JSON.stringify([
+                blockMock.base,
+                blockMock.base2,
+            ]),
+        }));
+        await page.route(buildApiUrl('homepage_txs'), (route) => route.fulfill({
+            status: 200,
+            body: JSON.stringify([
+                txMock.base,
+                txMock.withContractCreation,
+                txMock.withTokenTransfer,
+            ]),
+        }));
+        await page.route(buildApiUrl('homepage_chart_txs'), (route) => route.fulfill({
+            status: 200,
+            body: JSON.stringify(dailyTxsMock.base),
+        }));
 
-    const component = await mount(
-      <TestApp>
-        <Home/>
-      </TestApp>,
-    );
+        const component = await mount(
+            <TestApp>
+                <Home/>
+            </TestApp>,
+        );
 
-    await insertAdPlaceholder(page);
-
-    await expect(component.locator('main')).toHaveScreenshot();
-  });
+        await expect(component).toHaveScreenshot({
+            mask: [ page.locator(configs.adsBannerSelector) ],
+            maskColor: configs.maskColor,
+        });
+    });
 });
