5811f866886bb024fbba10ee5b01a1dc94770be6
tom goriunov <tom@ohhhh.me>
2023-09-19 17:18:07 +0300

    Highlight Safe wallets (#1174)
    * add tag and icon for safe contracts
    
    * surpress sentry error log and describe feature in the docs
    
    * refine logs at container start
    
    (cherry picked from commit db6c1650cce023d2fde651a0ca78bda25a3dd41f)

--- ui/shared/entities/address/AddressEntity.tsx
+++ ui/shared/entities/address/AddressEntity.tsx
@@ -8,6 +8,7 @@ import type { AddressParam } from 'types/api/addressParams';
 
 import { route } from 'nextjs-routes';
 
+import iconSafe from 'icons/brands/safe.svg';
 import iconContractVerified from 'icons/contract_verified.svg';
 import iconContract from 'icons/contract.svg';
 import * as EntityBase from 'ui/shared/entities/base/components';
@@ -17,119 +18,143 @@ import { getIconProps } from '../base/utils';
 type LinkProps = EntityBase.LinkBaseProps & Pick<EntityProps, 'address'>;
 
 const Link = chakra((props: LinkProps) => {
-  const defaultHref = route({ pathname: '/address/[hash]', query: { ...props.query, hash: props.address.hash } });
+    const defaultHref = route({ pathname: '/address/[hash]', query: { ...props.query, hash: props.address.hash } });
 
-  return (
-    <EntityBase.Link
-      { ...props }
-      href={ props.href ?? defaultHref }
-    >
-      { props.children }
-    </EntityBase.Link>
-  );
+    return (
+        <EntityBase.Link
+            { ...props }
+            href={ props.href ?? defaultHref }
+        >
+            { props.children }
+        </EntityBase.Link>
+    );
 });
 
-type IconProps = Pick<EntityProps, 'address' | 'isLoading' | 'iconSize'> & {
-  asProp?: As;
+type IconProps = Pick<EntityProps, 'address' | 'isLoading' | 'iconSize' | 'noIcon' | 'isSafeAddress'> & {
+    asProp?: As;
 };
 
 const Icon = (props: IconProps) => {
-  const styles = {
-    ...getIconProps(props.iconSize),
-    marginRight: 2,
-  };
+    if (props.noIcon) {
+        return null;
+    }
 
-  if (props.isLoading) {
-    return <Skeleton { ...styles } borderRadius="full" flexShrink={ 0 }/>;
-  }
+    const styles = {
+        ...getIconProps(props.iconSize),
+        marginRight: 2,
+    };
 
-  if (props.address.is_contract) {
-    if (props.address.is_verified) {
-      return (
-        <EntityBase.Icon
-          { ...props }
-          asProp={ iconContractVerified }
-          color="green.500"
-        />
-      );
+    if (props.isLoading) {
+        return <Skeleton { ...styles } borderRadius="full" flexShrink={ 0 }/>;
+    }
+
+    if (props.address.is_contract) {
+        if (props.isSafeAddress) {
+            return (
+                <EntityBase.Icon
+                    { ...props }
+                    asProp={ iconSafe }
+                />
+            );
+        }
+
+        if (props.address.is_verified) {
+            return (
+                <Tooltip label="Verified contract">
+          <span>
+            <EntityBase.Icon
+                { ...props }
+                asProp={ iconContractVerified }
+                color="green.500"
+                borderRadius={ 0 }
+            />
+          </span>
+                </Tooltip>
+            );
+        }
+
+        return (
+            <Tooltip label="Contract">
+        <span>
+          <EntityBase.Icon
+              { ...props }
+              asProp={ iconContract }
+              borderRadius={ 0 }
+          />
+        </span>
+            </Tooltip>
+        );
     }
 
     return (
-      <EntityBase.Icon
-        { ...props }
-        asProp={ iconContract }
-      />
+        <Tooltip label={ props.address.implementation_name }>
+            <Flex { ...styles }>
+                <Jazzicon diameter={ props.iconSize === 'lg' ? 30 : 20 } seed={ jsNumberForAddress(props.address.hash) }/>
+            </Flex>
+        </Tooltip>
     );
-  }
-
-  return (
-    <Tooltip label={ props.address.implementation_name }>
-      <Flex { ...styles }>
-        <Jazzicon diameter={ props.iconSize === 'lg' ? 30 : 20 } seed={ jsNumberForAddress(props.address.hash) }/>
-      </Flex>
-    </Tooltip>
-  );
 };
 
 type ContentProps = Omit<EntityBase.ContentBaseProps, 'text'> & Pick<EntityProps, 'address'>;
 
 const Content = chakra((props: ContentProps) => {
-  if (props.address.name) {
-    return (
-      <Tooltip label={ props.address.hash } maxW="100vw">
-        <Skeleton isLoaded={ !props.isLoading } overflow="hidden" textOverflow="ellipsis" whiteSpace="nowrap" as="span">
-          { props.address.name }
-        </Skeleton>
-      </Tooltip>
-    );
-  }
+    if (props.address.name) {
+        return (
+            <Tooltip label={ props.address.hash } maxW="100vw">
+                <Skeleton isLoaded={ !props.isLoading } overflow="hidden" textOverflow="ellipsis" whiteSpace="nowrap" as="span">
+                    { props.address.name }
+                </Skeleton>
+            </Tooltip>
+        );
+    }
 
-  return (
-    <EntityBase.Content
-      { ...props }
-      text={ props.address.hash }
-    />
-  );
+    return (
+        <EntityBase.Content
+            { ...props }
+            text={ props.address.hash }
+        />
+    );
 });
 
 type CopyProps = Omit<EntityBase.CopyBaseProps, 'text'> & Pick<EntityProps, 'address'>;
 
 const Copy = (props: CopyProps) => {
-  return (
-    <EntityBase.Copy
-      { ...props }
-      text={ props.address.hash }
-    />
-  );
+    return (
+        <EntityBase.Copy
+            { ...props }
+            text={ props.address.hash }
+        />
+    );
 };
 
 const Container = EntityBase.Container;
 
 export interface EntityProps extends EntityBase.EntityBaseProps {
-  address: Pick<AddressParam, 'hash' | 'name' | 'is_contract' | 'is_verified' | 'implementation_name'>;
+    address: Pick<AddressParam, 'hash' | 'name' | 'is_contract' | 'is_verified' | 'implementation_name'>;
+    isSafeAddress?: boolean;
 }
 
 const AddressEntry = (props: EntityProps) => {
-  const linkProps = _omit(props, [ 'className' ]);
-  const partsProps = _omit(props, [ 'className', 'onClick' ]);
+    const linkProps = _omit(props, [ 'className' ]);
+    const partsProps = _omit(props, [ 'className', 'onClick' ]);
 
-  return (
-    <Container className={ props.className }>
-      <Icon { ...partsProps }/>
-      <Link { ...linkProps }>
-        <Content { ...partsProps }/>
-      </Link>
-      <Copy { ...partsProps }/>
-    </Container>
-  );
+    return (
+        <Container className={ props.className }>
+            <Icon { ...partsProps }/>
+            <Link { ...linkProps }>
+                <Content { ...partsProps }/>
+            </Link>
+            <Copy { ...partsProps }/>
+        </Container>
+    );
 };
 
 export default React.memo(chakra(AddressEntry));
 
 export {
-  Container,
-  Link,
-  Icon,
-  Content,
-  Copy,
+    Container,
+    Link,
+    Icon,
+    Content,
+    Copy,
 };
